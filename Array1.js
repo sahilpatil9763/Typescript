ac51b20a9aa6c2a22f6c08a45c62afba9cc613c0
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-04-29 16:45:51 +0530

    Array.ts

--- Array1.js
+++ Array1.js
@@ -4,3 +4,7 @@ var Arr = [10, 20, 30, 40, 50];
 console.log("Length of array is : " + Arr.length);
 console.log("First element is : " + Arr[0]);
 console.log("Second element is : " + Arr[1]);
+var i = 0;
+for (i = 0; i < Arr.length; i++) {
+    console.log(Arr[i]);
+}
--- Array1.ts
+++ Array1.ts
@@ -6,3 +6,9 @@ var Arr:number[] = [10,20,30,40,50];
 console.log("Length of array is : "+Arr.length);
 console.log("First element is : "+Arr[0]);
 console.log("Second element is : "+Arr[1]);
+
+var i:number = 0;
+for(i=0; i<Arr.length; i++)
+{
+	console.log(Arr[i]);
+}
