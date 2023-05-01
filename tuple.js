a46fc637604be300f914efa416f37843d70c9ccd
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2023-05-01 17:43:42 +0530

    tuple

---
+++ tuple.js
@@ -0,0 +1,9 @@
+var Arr = [10, "Hello", true];
+console.log("Length of array is : " + Arr.length);
+console.log("First element is : " + Arr[0]);
+console.log("Second element is : " + Arr[1]);
+var i = 0;
+//  1	   2		3
+for (i = 0; i < Arr.length; i++) {
+    console.log(Arr[i]); //4
+}
---
+++ tuple.ts
@@ -0,0 +1,15 @@
+var Arr = [10,"Hello",true];
+
+console.log("Length of array is : "+Arr.length);
+console.log("First element is : "+Arr[0]);
+console.log("Second element is : "+Arr[1]);
+
+console.log("Elements form array are : ");
+
+var i:number = 0;
+
+//  1	   2		3
+for(i=0; i<Arr.length; i++)
+{
+	console.log(Arr[i]);	//4
+}
