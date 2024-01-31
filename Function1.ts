302b5759da3dd41ceb0b601c792ebe6196ec3877
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2024-01-31 23:35:51 +0530

    HTML, JS & CSS files updated
    updated JS file and made changes in HTML and CSS files

---
+++ Function1.ts
@@ -0,0 +1,17 @@
+function fun():void      // Function defination
+{
+    console.log("Inside fun");
+}
+
+fun();              // Function call
+
+/*
+
+    void fun()
+    {
+        print("Inside fun");
+        cout << "Inside fun";
+        System.out.println("Inside fun");
+    }
+
+*/  /// It is used in C, C++, Java
